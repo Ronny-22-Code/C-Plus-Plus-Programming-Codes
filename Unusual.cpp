0c24e089e0103a922434144faa1f205dbabae84f
Rohan Sethi <mailtorohansethi@gmail.com>
2019-02-08 23:34:47 +0530

    Unusual Sum

---
+++ Unusual.cpp
@@ -0,0 +1,26 @@
+//This program is to do something unusual.
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+	
+	int a=0,s=0;
+	
+	cout<<"Enter the value of a:";
+	
+	cin>>a;	
+	
+	s = a+(a*10+a) + (a*100+a) + (a*1000+a) ;	
+
+		
+cout<<"The sum of this unusual output is:"<<s;
+
+return(0);
+
+}
