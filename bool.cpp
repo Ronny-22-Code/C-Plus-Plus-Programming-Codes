d5e2d82dcb610bfcbabc090f57ed8d8a2b10a074
Rohan Sethi <mailtorohansethi@gmail.com>
2019-03-06 00:00:28 +0530

    A typical example of bool data type.

---
+++ bool.cpp
@@ -0,0 +1,24 @@
+// This program is to return a bool value.
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	bool c ;
+	int a=1,b=3;
+	
+	if(b>a)
+	{
+		cout<<c;
+		
+	}
+	
+	return(0);
+		
+	
+	
+}
+
