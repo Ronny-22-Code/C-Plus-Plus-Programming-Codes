e36930b4f05b7accfbe74ebc9dbda84428d131af
Rohan Sethi <mailtorohansethi@gmail.com>
2019-03-08 22:54:05 +0530

    This source code file returns the square values .
    This file returns the square values between 6 and 20.

---
+++ Squary.cpp
@@ -0,0 +1,22 @@
+//This program is to return the squared values of the elements.
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int i;
+	
+	for(i=6;i<=20;i++)
+	{
+		
+		cout<<i*i<<" ";
+		
+	}
+
+return(0);
+
+}
+
